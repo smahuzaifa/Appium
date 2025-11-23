4d5db33deed690e4874a53077be88bcb9db8f983
smahuzaifa <huzaifasadath@gmail.com>
2025-11-23 13:14:27 +1100

    Removed unwanted folders

--- src/test/java/AppiumBasics.java
+++ src/test/java/AppiumBasics.java
@@ -1,8 +1,16 @@
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
 import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class AppiumBasics{
     @Test
-    public void appiumTest(){
-        
+    public void appiumTest() throws MalformedURLException {
+        UiAutomator2Options options = new UiAutomator2Options();
+        options.setDeviceName("");
+
+        AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/"),options);
     }
 }
